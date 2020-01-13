8586122857d92744135af22d14f386c1b5ce8261
Elliot Ronaghan <ronawho@gmail.com>
2020-01-13 13:39:33 -0500

    Arkouda graphs

---
+++ graphdata.js
@@ -0,0 +1,78 @@
+// AUTOMATICALLY GENERATED GRAPH DESCRIPTION
+document.title = "Arkouda Performance Graphs for 16 node XC";
+var pageTitle = "Arkouda Performance Graphs for 16 node XC";
+var runDate= "2020-01-13";
+var numericX = false;
+var configurations = [];
+var configurationsVis = [];
+var allGraphs = [
+{
+   "title" : "Stream Performance",
+   "suites" : ["All"],
+   "datfname" : "arkouda0.json",
+   "ylabel" : "Performance (GiB/S)",
+   "startdate" : "2020-01-11",
+   "enddate" : "2020-01-13",
+   "displayrange" : true,
+   "defaultexpand" : false,
+   "annotations" : []
+},
+{
+   "title" : "Argsort Performance",
+   "suites" : ["All"],
+   "datfname" : "arkouda1.json",
+   "ylabel" : "Performance (GiB/S)",
+   "startdate" : "2020-01-11",
+   "enddate" : "2020-01-13",
+   "displayrange" : true,
+   "defaultexpand" : false,
+   "annotations" : []
+},
+{
+   "title" : "Gather Performance",
+   "suites" : ["All"],
+   "datfname" : "arkouda2.json",
+   "ylabel" : "Performance (GiB/S)",
+   "startdate" : "2020-01-11",
+   "enddate" : "2020-01-13",
+   "displayrange" : true,
+   "defaultexpand" : false,
+   "annotations" : []
+},
+{
+   "title" : "Scatter Performance",
+   "suites" : ["All"],
+   "datfname" : "arkouda3.json",
+   "ylabel" : "Performance (GiB/S)",
+   "startdate" : "2020-01-11",
+   "enddate" : "2020-01-13",
+   "displayrange" : true,
+   "defaultexpand" : false,
+   "annotations" : []
+},
+{
+   "title" : "Scan Performance",
+   "suites" : ["All"],
+   "datfname" : "arkouda4.json",
+   "ylabel" : "Performance (GiB/S)",
+   "startdate" : "2020-01-11",
+   "enddate" : "2020-01-13",
+   "displayrange" : true,
+   "defaultexpand" : false,
+   "annotations" : []
+},
+{
+   "title" : "Reduce Performance",
+   "suites" : ["All"],
+   "datfname" : "arkouda5.json",
+   "ylabel" : "Performance (GiB/S)",
+   "startdate" : "2020-01-11",
+   "enddate" : "2020-01-13",
+   "displayrange" : true,
+   "defaultexpand" : false,
+   "annotations" : []
+}
+];
+var perfSuites = [
+{ "suite" : "All" }
+];
