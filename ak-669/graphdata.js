c4dc23078af21ef6c1e7555fc47f211cc4d004c2
Elliot Ronaghan <ronawho@gmail.com>
2021-02-22 11:20:58 -0500

    Add graphs for https://github.com/mhmerrill/arkouda/pull/669

---
+++ ak-669/graphdata.js
@@ -0,0 +1,245 @@
+// AUTOMATICALLY GENERATED GRAPH DESCRIPTION
+document.title = "Arkouda Performance Graphs";
+var pageTitle = "Arkouda Performance Graphs";
+var runDate= "2021-02-22";
+var numericX = false;
+var configurations = [];
+var configurationsVis = [];
+var allGraphs = [
+{
+   "title" : "Stream Performance",
+   "suites" : ["Benchmarks"],
+   "datfname" : "arkouda0.json",
+   "ylabel" : "Performance (GiB/s)",
+   "startdate" : "2021-02-22",
+   "enddate" : "2021-02-22",
+   "displayrange" : true,
+   "defaultexpand" : false,
+   "annotations" : []
+},
+{
+   "title" : "Argsort Performance",
+   "suites" : ["Benchmarks"],
+   "datfname" : "arkouda1.json",
+   "ylabel" : "Performance (GiB/s)",
+   "startdate" : "2021-02-22",
+   "enddate" : "2021-02-22",
+   "displayrange" : true,
+   "defaultexpand" : false,
+   "annotations" : []
+},
+{
+   "title" : "Coargsort Performance",
+   "suites" : ["Benchmarks"],
+   "datfname" : "arkouda2.json",
+   "ylabel" : "Performance (GiB/S)",
+   "startdate" : "2021-02-22",
+   "enddate" : "2021-02-22",
+   "displayrange" : true,
+   "defaultexpand" : false,
+   "annotations" : []
+},
+{
+   "title" : "Groupby Performance",
+   "suites" : ["Benchmarks"],
+   "datfname" : "arkouda3.json",
+   "ylabel" : "Performance (GiB/S)",
+   "startdate" : "2021-02-22",
+   "enddate" : "2021-02-22",
+   "displayrange" : true,
+   "defaultexpand" : false,
+   "annotations" : []
+},
+{
+   "title" : "Grouped Aggregation Performance (Tier 1)",
+   "suites" : ["Benchmarks"],
+   "datfname" : "arkouda4.json",
+   "ylabel" : "Performance (GiB/s)",
+   "startdate" : "2021-02-21",
+   "enddate" : "2021-02-22",
+   "displayrange" : true,
+   "defaultexpand" : false,
+   "annotations" : []
+},
+{
+   "title" : "Grouped Aggregation Performance (Tier 2)",
+   "suites" : ["Benchmarks"],
+   "datfname" : "arkouda5.json",
+   "ylabel" : "Performance (GiB/s)",
+   "startdate" : "2021-02-21",
+   "enddate" : "2021-02-22",
+   "displayrange" : true,
+   "defaultexpand" : false,
+   "annotations" : []
+},
+{
+   "title" : "Grouped Aggregation Performance (Tier 3)",
+   "suites" : ["Benchmarks"],
+   "datfname" : "arkouda6.json",
+   "ylabel" : "Performance (GiB/s)",
+   "startdate" : "2021-02-21",
+   "enddate" : "2021-02-22",
+   "displayrange" : true,
+   "defaultexpand" : false,
+   "annotations" : []
+},
+{
+   "title" : "Gather Performance",
+   "suites" : ["Benchmarks"],
+   "datfname" : "arkouda7.json",
+   "ylabel" : "Performance (GiB/s)",
+   "startdate" : "2021-02-22",
+   "enddate" : "2021-02-22",
+   "displayrange" : true,
+   "defaultexpand" : false,
+   "annotations" : []
+},
+{
+   "title" : "Scatter Performance",
+   "suites" : ["Benchmarks"],
+   "datfname" : "arkouda8.json",
+   "ylabel" : "Performance (GiB/s)",
+   "startdate" : "2021-02-22",
+   "enddate" : "2021-02-22",
+   "displayrange" : true,
+   "defaultexpand" : false,
+   "annotations" : []
+},
+{
+   "title" : "Scan Performance",
+   "suites" : ["Benchmarks"],
+   "datfname" : "arkouda9.json",
+   "ylabel" : "Performance (GiB/s)",
+   "startdate" : "2021-02-22",
+   "enddate" : "2021-02-22",
+   "displayrange" : true,
+   "defaultexpand" : false,
+   "annotations" : []
+},
+{
+   "title" : "Reduce Performance",
+   "suites" : ["Benchmarks"],
+   "datfname" : "arkouda10.json",
+   "ylabel" : "Performance (GiB/s)",
+   "startdate" : "2021-02-22",
+   "enddate" : "2021-02-22",
+   "displayrange" : true,
+   "defaultexpand" : false,
+   "annotations" : []
+},
+{
+   "title" : "Set Operations Performance",
+   "suites" : ["Benchmarks"],
+   "datfname" : "arkouda11.json",
+   "ylabel" : "Performance (GiB/s)",
+   "startdate" : "2021-02-22",
+   "enddate" : "2021-02-22",
+   "displayrange" : true,
+   "defaultexpand" : false,
+   "annotations" : []
+},
+{
+   "title" : "Array Creation Performance",
+   "suites" : ["Benchmarks"],
+   "datfname" : "arkouda12.json",
+   "ylabel" : "Performance (GiB/s)",
+   "startdate" : "2021-02-22",
+   "enddate" : "2021-02-22",
+   "displayrange" : true,
+   "defaultexpand" : false,
+   "annotations" : []
+},
+{
+   "title" : "IO Performance",
+   "suites" : ["Benchmarks"],
+   "datfname" : "arkouda13.json",
+   "ylabel" : "Performance (GiB/s)",
+   "startdate" : "2021-02-22",
+   "enddate" : "2021-02-22",
+   "displayrange" : true,
+   "defaultexpand" : false,
+   "annotations" : []
+},
+{
+   "title" : "Noop Performance",
+   "suites" : ["Benchmarks"],
+   "datfname" : "arkouda14.json",
+   "ylabel" : "Performance (ops/s)",
+   "startdate" : "2021-02-22",
+   "enddate" : "2021-02-22",
+   "displayrange" : true,
+   "defaultexpand" : false,
+   "annotations" : []
+},
+{
+   "title" : "String Argsort Performance",
+   "suites" : ["String Benchmarks"],
+   "datfname" : "arkouda-string0.json",
+   "ylabel" : "Performance (GiB/s)",
+   "startdate" : "2021-02-22",
+   "enddate" : "2021-02-22",
+   "displayrange" : true,
+   "defaultexpand" : false,
+   "annotations" : []
+},
+{
+   "title" : "String Coargsort Performance",
+   "suites" : ["String Benchmarks"],
+   "datfname" : "arkouda-string1.json",
+   "ylabel" : "Performance (GiB/S)",
+   "startdate" : "2021-02-22",
+   "enddate" : "2021-02-22",
+   "displayrange" : true,
+   "defaultexpand" : false,
+   "annotations" : []
+},
+{
+   "title" : "String Groupby Performance",
+   "suites" : ["String Benchmarks"],
+   "datfname" : "arkouda-string2.json",
+   "ylabel" : "Performance (GiB/S)",
+   "startdate" : "2021-02-22",
+   "enddate" : "2021-02-22",
+   "displayrange" : true,
+   "defaultexpand" : false,
+   "annotations" : []
+},
+{
+   "title" : "String Gather Performance",
+   "suites" : ["String Benchmarks"],
+   "datfname" : "arkouda-string3.json",
+   "ylabel" : "Performance (GiB/s)",
+   "startdate" : "2021-02-22",
+   "enddate" : "2021-02-22",
+   "displayrange" : true,
+   "defaultexpand" : false,
+   "annotations" : []
+},
+{
+   "title" : "Build Time",
+   "suites" : ["Build Stats"],
+   "datfname" : "arkouda-comp0.json",
+   "ylabel" : "Time (sec)",
+   "startdate" : "2021-02-22",
+   "enddate" : "2021-02-22",
+   "displayrange" : true,
+   "defaultexpand" : false,
+   "annotations" : []
+},
+{
+   "title" : "Emitted Code Size",
+   "suites" : ["Build Stats"],
+   "datfname" : "arkouda-comp1.json",
+   "ylabel" : "Statements",
+   "startdate" : "2021-02-22",
+   "enddate" : "2021-02-22",
+   "displayrange" : true,
+   "defaultexpand" : false,
+   "annotations" : []
+}
+];
+var perfSuites = [
+{ "suite" : "Benchmarks" },
+{ "suite" : "String Benchmarks" },
+{ "suite" : "Build Stats" }
+];
